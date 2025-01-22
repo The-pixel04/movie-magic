2932e9000316bdc13debdda0e7cae7519e8b0d4c
The-pixel04 <krsyrdyrd@gmail.com>
2025-01-22 18:26:07 +0200

    Create movie model

---
+++ src/models/Movie.js
@@ -0,0 +1,15 @@
+import { Schema, model } from "mongoose";
+
+const movieSchema = new Schema({
+    title: String,
+    category: String,
+    director: String,
+    year: Number,
+    imageUrl: String,
+    rating: Number,
+    description: String
+});
+
+const Movie = model('Movie', movieSchema);
+
+export default Movie;
