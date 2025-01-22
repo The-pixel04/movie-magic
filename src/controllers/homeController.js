39b351ca902d64acb44816a085d9026e89b84e3f
The-pixel04 <krsyrdyrd@gmail.com>
2025-01-22 19:07:40 +0200

    Refactor homepage to use database movies

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -2,10 +2,13 @@ import express from 'express';
 import movieService from '../services/movieService.js';
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    let movies = movieService.getAll();
-    res.render('home', { movies })
+router.get('/', async (req, res) => {
+    let movies = await movieService.getAll();
+
+    res.render('home', { movies });
+
 });
+
 router.get('/about', (req, res) => {
     res.render('about')
 });
--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,20 +1,24 @@
-import movies from "../movies.js";
+import Movie from "../models/Movie.js";
+// import movies from "../movies.js";
 import { v4 as uuid } from 'uuid'
 
 export default {
-    findMovie(movieId) {
-        const movie = movies.find(movie => movie.id == movieId);
+    getMovie(movieId) {
+        const movie = Movie.findById(movieId);
         return movie
     },
     createMovie(movie) {
-        let id = uuid();
-        movies.push({
-            id: id,
-            ...movie,
-            rating: Number(movie.rating)
-        });
+        // let id = uuid();
+        // movies.push({
+        //     id: id,
+        //     ...movie,
+        //     rating: Number(movie.rating)
+        // });
     },
-    getAll() {
-        return movies
+    getAll(filter = {}) {
+        let result = Movie.find({});
+        console.log(result)
+
+        return result
     }
 }
