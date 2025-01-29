003325ce8a9b205c063215926c0917e414524b01
The-pixel04 <krsyrdyrd@gmail.com>
2025-01-29 19:10:44 +0200

    Add login page

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -10,6 +10,11 @@ authController.get('/register', (req, res) => {
 authController.post('/register', async (req, res) => {
     const userData = req.body;
     await authService.register(userData);
+    res.redirect('/auth/login');
+});
+
+authController.get('/login', (req, res) => {
+    res.render('auth/login');
 });
 
 export default authController;
